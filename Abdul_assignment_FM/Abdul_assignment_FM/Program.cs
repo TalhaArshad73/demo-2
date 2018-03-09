5e3c6eaea62c581f6a251d4a3d6f704664513a89
Talha Arshad <talharan73@gmail.com>
2018-03-10 00:32:40 +0500

    abdi=

---
+++ Abdul_assignment_FM/Abdul_assignment_FM/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abdul_assignment_FM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string input = "Pakistan";
+            
+
+            char [] chars=input.ToCharArray();
+            int len = chars.Length;
+            Console.WriteLine("Output is:");
+            for (int i = 0; i < len; i++)
+            {
+               chars[i] =(char)((int) chars[i] + 2);
+               Console.Write(chars[i] + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
